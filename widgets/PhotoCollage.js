4e0e306d063876285761d4a9c1d93ef9f815f5a0
Daniel Speyer <dspeyer@gmail.com>
2020-09-08 22:15:38 -0400

    move getPhoto to lib.js

--- widgets/PhotoCollage.js
+++ widgets/PhotoCollage.js
@@ -1,4 +1,4 @@
-import { putOnBox } from '../../lib.js';
+import { putOnBox, getPhoto } from '../../lib.js';
 
 function dbg(txt) {
 //    $('<div>').text(txt).css('color','white').appendTo($('body'));
@@ -18,65 +18,15 @@ export class PhotoCollage {
         });
     }
     async getPhoto() {
-        let stream = await navigator.mediaDevices.getUserMedia({ video: true });
-        let settings = stream.getVideoTracks()[0].getSettings();
-        let {width,height} = settings;
-        if (width>height && window.innerWidth<window.innerHeight) {
-            // Probable browser bug in video dimension handling
-            [width,height] = [height,width];
-        }
-        let w,h,l,t;
-        dbg('settings(width='+width+',height='+height+') div(width='+this.div.width()+',height='+this.div.height()+')');
-        if (width/height > this.div.width()/this.div.height()) {
-            w = this.div.width();
-            h = this.div.width() * height / width;
-            t = (this.div.height() - h) / 2;
-            l = 0;
-            dbg('horiz setup: w='+w+' h='+h);
-        } else {
-            h = this.div.height();
-            w = this.div.height() * width / height;
-            l = (this.div.width() - w) / 2;
-            t = 0;
-            dbg('vert setup: w='+w+' h='+h);
-        }
-        let video = $('<video>').
-            css('position','absolute').
-            css('width',w+'px').
-            css('height',h+'px').
-            css('left',l+'px').
-            css('top',t+'px').
-            css('z-index',999).
-            appendTo(this.div);
-        video[0].srcObject = stream;
-        video[0].play();
-        let button = $('<input type="button" value="Take">').
-            css('position','absolute').
-            css('bottom','0').
-            css('left','40%').
-            css('right','40%').
-            css('padding','2em').
-            css('z-index','1000').
-            appendTo(this.div);
-        button.click(async ()=>{
-            dbg('w='+w+' h='+h);
-            let canvas = $('<canvas width='+w+' height='+h+'>').appendTo($('body'));
-            let context = canvas[0].getContext('2d');
-            context.drawImage(video[0], 0, 0, w, h);
-            canvas[0].toBlob((blob)=>{
-                let fd = new FormData();
-                fd.append('img', blob);
-                $.ajax({
-                    type: 'POST',
-                    url: 'widgetData',
-                    data: fd,
-                    processData: false,
-                    contentType: false
-                });
-            }, 'image/jpeg');
-            canvas.remove();
-            video.remove();
-            button.remove();
+        let blob = await getPhoto(this.div);
+        let fd = new FormData();
+        fd.append('img', blob);
+        $.ajax({
+            type: 'POST',
+            url: 'widgetData',
+            data: fd,
+            processData: false,
+            contentType: false
         });
     }
     from_server(data) {
