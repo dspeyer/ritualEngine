299315e8eb2bc1708d868c1421cb836cc7d468b0
Taymon A. Beal <taymon@google.com>
2020-11-27 22:19:16 -0500

    Use prop instead of attr to mute participant audio

--- html/lib.js
+++ html/lib.js
@@ -288,7 +288,7 @@ export async function showParticipantVideo(roomId, token, useParticipantAudio) {
 
 export function setMuted(mut) {
     muted = mut;
-    $('.participant-audio').attr('muted', muted);
+    $('.participant-audio').prop('muted', muted);
     if (localAudio) {
         if (mut) {
             localAudio.disable();
@@ -305,7 +305,7 @@ function addVideoCircle(track) {
 }
 
 function listenToAudio(track) {
-    $(track.attach()).addClass('participant-audio').attr('muted', muted).appendTo('body');
+    $(track.attach()).addClass('participant-audio').prop('muted', muted).appendTo('body');
 }
 
 export function setZoomMute(v) {} // TODO: something
