9f8762a9434613baa7c93d6aee9fe2ac26d5c2e7
Daniel Speyer <dspeyer@gmail.com>
2020-12-12 03:14:01 -0500

    styling on WelcomeWatch

--- widgets/WelcomeWatch.js
+++ widgets/WelcomeWatch.js
@@ -4,6 +4,11 @@ export class WelcomeWatch {
     constructor({boxColor}) {
         this.div = $('<div>').appendTo($('body'));
         putOnBox(this.div, boxColor);
+        this.div.css({background: 'rgba(255,255,255,0.6)',
+                      boxShadow: '0 0 5px white',
+                      borderRadius: '10px',
+                      textAlign: 'center',
+                      paddingTop: this.div.height()/2 + 'px'});
     }
     from_server({n}) {
         this.div.text(n+' clients being welcomed');
