65d8410fb8528ecb89380c2c26fa2cbb80ea60fa
Raemon <raemon777@gmail.com>
2020-12-14 21:37:17 -0800

    solstice2020 dec 14

--- widgets/WelcomeWatch.js
+++ widgets/WelcomeWatch.js
@@ -4,14 +4,10 @@ export class WelcomeWatch {
     constructor({boxColor}) {
         this.div = $('<div>').appendTo($('body'));
         putOnBox(this.div, boxColor);
-        this.div.css({background: 'rgba(255,255,255,0.6)',
-                      boxShadow: '0 0 5px white',
-                      borderRadius: '10px',
-                      textAlign: 'center',
-                      paddingTop: this.div.height()/2 + 'px'});
+        this.clientWatch = $('<div class="client-watch">').appendTo($(this.div));
     }
     from_server({n}) {
-        this.div.text(n+' clients being welcomed');
+        this.clientWatch.text(n+' clients being welcomed');
     }
     destroy() {
         this.div.remove();
